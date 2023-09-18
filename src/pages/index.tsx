cf021769fad6c18393a2b84bc3ae96a85cc65749
Nahum Sá <nahumsaa@gmail.com>
2023-09-17 23:34:31 -0300

    add form to register the email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,30 @@ export default function Home() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Hi</h1>
-        <Link
-           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href="/new">New</Link>
+        <h1 className="text-3xl">Alerta de preços para o site Madeira Madeira</h1>
       </header>
+       <div>
+      <form method="POST" action="https://www.formbackend.com/f/664decaabbf1c319">
+        <div>
+          <label>Link para o produto:</label>
+          <input type="text" name="name" />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input type="text" name="email" />
+        </div>
+
+        <div>
+          <label>Preço para alertar</label>
+          <input name="message"></input>
+        </div>
+
+        <button
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            type="submit">Registrar</button>
+      </form>
+    </div>
     </>
   )
 }

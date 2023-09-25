7b579e70f5772652df2c236c681b3d231b03dc68
Nahum Sá <nahumsaa@gmail.com>
2023-09-25 19:03:04 -0300

    add backend to deploy to shuttle

---
+++ backend/src/main.rs
@@ -0,0 +1,12 @@
+use axum::Router;
+use std::path::PathBuf;
+use tower_http::services::ServeDir;
+
+#[shuttle_runtime::main]
+async fn axum(
+    #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
+) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().nest_service("/", ServeDir::new(static_folder));
+
+    Ok(router.into())
+}
